e3045e8cf54f2479f8e539282c0c4233d4b863d0
GuaiYiHu <guaiyihu@foxmail.com>
2019-09-14 13:56:58 +0800

    X00T: Add missing dependency for sound trigger hal
    Change-Id: Ia16f5e6c50cb595a5a37b532097e780ae0668c56

--- extract-files.sh
+++ extract-files.sh
@@ -64,3 +64,7 @@ extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 # Load camera.sdm660.so shim
 CAM_SDM660="$DEVICE_BLOB_ROOT"/vendor/lib/hw/camera.sdm660.so
 patchelf --add-needed camera.sdm660_shim.so "$CAM_SDM660"
+
+# Dependency for sound trigger hal
+patchelf --add-needed libprocessgroup.so "$BLOB_ROOT"/vendor/lib/hw/sound_trigger.primary.sdm660.so
+patchelf --add-needed libprocessgroup.so "$BLOB_ROOT"/vendor/lib64/hw/sound_trigger.primary.sdm660.so
