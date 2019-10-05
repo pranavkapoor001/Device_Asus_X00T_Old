27e25e9b7ee04ed49c876365e1e1b51e6d499c2f
Michael Bestas <mkbestas@lineageos.org>
2019-10-05 22:50:26 +0300

    X00T: Update to power HAL 1.2
    Change-Id: Ic8c5f7e9c77a95befff5661e1634683ec6c25ac4

--- device.mk
+++ device.mk
@@ -358,7 +358,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
