61f4466279a31a516c814739e81e70e41d2ad735
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-01-12 18:39:36 +0530

    Revert "X00T: Symlink libtinycompress as libtinycompress_vendor"
    This reverts commit dbf03e5c8c62bebe6013c487be6e19352c1b7c05.

--- Android.mk
+++ Android.mk
@@ -112,23 +112,6 @@ $(BT_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(BT_FIRMWARE_SYMLINKS)
 
-# libtinycompress symlink for audio hal
-TINYCOMPRESS_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libtinycompress_vendor.so
-$(TINYCOMPRESS_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@echo "libtinycompress shared object link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf libtinycompress.so $@
-
-TINYCOMPRESS64_SYMLINK := $(TARGET_OUT_VENDOR)/lib64/libtinycompress_vendor.so
-$(TINYCOMPRESS64_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@echo "libtinycompress_64 shared object link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf libtinycompress.so $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(TINYCOMPRESS_SYMLINK) $(TINYCOMPRESS64_SYMLINK)
-
 EGL_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libGLESv2_adreno.so
 $(EGL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	@mkdir -p $(dir $@)
